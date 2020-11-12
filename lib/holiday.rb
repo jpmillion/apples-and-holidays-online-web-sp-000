88d6743074c647a1f616239d3ee972fe02e4dbd0
John Million <jpmillion@comcast.net>
2020-11-12 16:46:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.values.collect do |item|
-    
+    item.select do |k, v|
 end
 
 
